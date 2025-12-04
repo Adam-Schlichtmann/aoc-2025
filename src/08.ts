0d00d82bcb714979add85c891a0c06ecef4c0a17
Adam-Schlichtmann <adam.schlichtmann1113@gmail.com>
2025-12-03 22:05:29 -0600

    Template out remaining days with new structure

--- src/08.ts
+++ src/08.ts
@@ -1,8 +1,44 @@
-const part1 = () => {};
+import { readFileSync } from "fs";
 
-const part2 = () => {};
+const readFile = (file: string) => {
+  const content = readFileSync(file, "utf-8");
+  const lines = content.split("\n").filter(Boolean);
+  return lines;
+};
+
+const parseLine = (line: string) => {
+  return line;
+};
+
+const part1 = (file: string): number => {
+  const lines = readFile(file);
+  return 0;
+};
+
+const part2 = (file: string): number => {
+  const lines = readFile(file);
+  return 0;
+};
 
 export default () => {
-  part1();
-  part2();
+  const testAnswer1 = part1("./src/input/08.test.txt");
+  if (testAnswer1 === 0) {
+    console.log("Part 1 Answer:", part1("./src/input/08.txt"));
+  } else {
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      "Part 1 failed to pass test input. Got",
+      testAnswer1
+    );
+  }
+  const testAnswer2 = part2("./src/input/08.test.txt");
+  if (testAnswer2 === 0) {
+    console.log("Part 2 Answer:", part2("./src/input/08.txt"));
+  } else {
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      "Part 2 failed to pass test input. Got",
+      testAnswer2
+    );
+  }
 };
