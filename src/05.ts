c72b5a383f52e1aa92b50bfad7c5be775368b9ca
Adam-Schlichtmann <adam.schlichtmann1113@gmail.com>
2025-12-07 15:11:50 -0600

    Day 5

--- src/05.ts
+++ src/05.ts
@@ -2,27 +2,85 @@ import { readFileSync } from "fs";
 
 const readFile = (file: string) => {
   const content = readFileSync(file, "utf-8");
-  const lines = content.split("\n").filter(Boolean);
+  const lines = content.split("\n\n").filter(Boolean);
   return lines;
 };
 
-const parseLine = (line: string) => {
-  return line;
+type Range = { start: number; end: number };
+
+const parseFresh = (ranges: string): Range[] => {
+  const lines = ranges.split("\n").filter(Boolean);
+  return lines.map(parseRangeLine);
+};
+
+const parseRangeLine = (line: string): Range => {
+  const [start, end] = line.split("-");
+  return { start: Number(start), end: Number(end) };
+};
+
+const parseIngredients = (ingredients: string): number[] => {
+  return ingredients.split("\n").filter(Boolean).map(Number);
+};
+
+const inRange = (n: number, range: Range) => n >= range.start && n <= range.end;
+
+const collapseRanges = (ranges: Range[]): Range[] => {
+  const newRanges: Range[] = [];
+
+  const sortedByStart = ranges.sort((a, b) => {
+    if (a.start === b.start) {
+      return a.end - b.end;
+    }
+    return a.start - b.start;
+  });
+  let i = 0;
+  while (i < sortedByStart.length) {
+    let j = i + 1;
+    let newRange: Range = { ...sortedByStart[i] };
+    while (
+      j < sortedByStart.length &&
+      inRange(sortedByStart[j].start, newRange)
+    ) {
+      if (newRange.end < sortedByStart[j].end) {
+        newRange.end = sortedByStart[j].end;
+      }
+      j++;
+    }
+
+    newRanges.push(newRange);
+    i = j;
+  }
+
+  return newRanges;
 };
 
 const part1 = (file: string): number => {
-  const lines = readFile(file);
-  return 0;
+  const [rawRanges, rawIngredients] = readFile(file);
+  const ranges = parseFresh(rawRanges);
+  const ingredients = parseIngredients(rawIngredients);
+  let count = 0;
+  for (const i of ingredients) {
+    if (ranges.some((r) => i >= r.start && i <= r.end)) {
+      count++;
+    }
+  }
+  return count;
 };
 
 const part2 = (file: string): number => {
-  const lines = readFile(file);
-  return 0;
+  const [rawRanges] = readFile(file);
+  const ranges = parseFresh(rawRanges);
+  const collapsedRanges = collapseRanges(ranges);
+  let count = 0;
+  for (const range of collapsedRanges) {
+    count += range.end - range.start + 1;
+  }
+  return count;
 };
 
 export default () => {
   const testAnswer1 = part1("./src/input/05.test.txt");
-  if (testAnswer1 === 0) {
+  if (testAnswer1 === 3) {
     console.log("Part 1 Answer:", part1("./src/input/05.txt"));
   } else {
     console.log(
@@ -32,7 +90,7 @@ export default () => {
     );
   }
   const testAnswer2 = part2("./src/input/05.test.txt");
-  if (testAnswer2 === 0) {
+  if (testAnswer2 === 14) {
     console.log("Part 2 Answer:", part2("./src/input/05.txt"));
   } else {
     console.log(
